a66e55acb873fd9dac213fbfca9a1ecb527c929f
AnikaJ33 <anikajain33@gmail.com>
2024-09-24 21:06:59 -0400

    Read a DNA file

---
+++ DNA_Sequence_Analyzer.py
@@ -0,0 +1,16 @@
+""""
+DNA Sequence Analyzer
+This project involves creating a program that can analyze DNA sequences. 
+1. Read DNA sequences from a file or accept user input.
+Calculate basic statistics like GC content and sequence length.
+Find and count specific nucleotide patterns or motifs.
+Transcribe DNA to RNA.
+Translate RNA to amino acid sequences.
+Enter multiple DNA sequences and find common motifs between the sequences
+Make a hidden MARKOV model
+"""
+
+#Take user input of a DNA sequence
+dna_seq = input("Enter a DNA sequence: ")
+print(dna_seq)
+
