f0694b14dd17684e893bbaac54a7eafef07b84dd
AnikaJ33 <anikajain33@gmail.com>
2025-01-09 20:11:23 -0500

    Count number of nucleotides

--- DNA_Sequence_Analyzer.py
+++ DNA_Sequence_Analyzer.py
@@ -14,3 +14,19 @@ Make a hidden MARKOV model
 dna_seq = input("Enter a DNA sequence: ")
 print(dna_seq)
 
+#Count number of times A,C,G,T occur in the DNA sequence
+count_A = 0
+count_C = 0
+count_G = 0
+count_T = 0
+
+for nuc in dna_seq:
+    if nuc == 'A':
+        count_A += 1
+    elif nuc == 'C':
+        count_C += 1
+    elif nuc == 'G':        
+        count_G += 1
+    elif nuc == 'T':
+        count_T += 1
+print(count_A, count_C, count_G, count_T)
