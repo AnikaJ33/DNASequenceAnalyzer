e406a1bf1c2617b28e21bcc7ed2656f5d4411f66
AnikaJ33 <anikajain33@gmail.com>
2025-05-30 21:09:09 -0400

    Reads a user-input sequence and fasta input

--- DNA_Sequence_Analyzer.py
+++ DNA_Sequence_Analyzer.py
@@ -16,24 +16,48 @@ Build a codon translator that converts DNA to amino acid sequences using the gen
 Advanced Extensions:
 Implement ORF (Open Reading Frame) finding to locate potential genes, add basic sequence alignment for comparing related sequences, or create a primer design tool for PCR applications.
 """
+import argparse
 
 #Take user input of a DNA sequence
-dna_seq = input("Enter a DNA sequence: ")
-print(dna_seq)
+def read_fasta(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+        print(lines)
+        print(len(lines))
 
-#Count number of times A,C,G,T occur in the DNA sequence
-count_A = 0
-count_C = 0
-count_G = 0
-count_T = 0
 
-for nuc in dna_seq:
-    if nuc == 'A':
-        count_A += 1
-    elif nuc == 'C':
-        count_C += 1
-    elif nuc == 'G':        
-        count_G += 1
-    elif nuc == 'T':
-        count_T += 1
-print(count_A, count_C, count_G, count_T)
+def main():
+    parser = argparse.ArgumentParser(prog="DNA Sequence Analyzer",
+                                     description="Analyze DNA sequences from a FASTA file or user input.")
+    parser.add_argument('-f','--file',type=str, help="Path to the FASTA file containing DNA sequences.")
+    parser.add_argument('-i','--input',action='strore_true', help="Accept user input for DNA sequence.")
+    args = parser.parse_args()
+
+if __name__ == "__main__":
+    main()
+
+
+
+#     file_path = input("Enter the path to the FASTA file: ")
+#     read_fasta(file_path)
+    
+
+# dna_seq = input("Enter a DNA sequence: ")
+# print(dna_seq)
+
+# #Count number of times A,C,G,T occur in the DNA sequence
+# count_A = 0
+# count_C = 0
+# count_G = 0
+# count_T = 0
+
+# for nuc in dna_seq:
+#     if nuc == 'A':
+#         count_A += 1
+#     elif nuc == 'C':
+#         count_C += 1
+#     elif nuc == 'G':        
+#         count_G += 1
+#     elif nuc == 'T':
+#         count_T += 1
+# print(count_A, count_C, count_G, count_T)
